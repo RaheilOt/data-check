f4282be8011f5d2f83b403a2b6d277a5202ffd8e
raheil <raheil@mak-bwk-alkhas-b-raheil.local>
2025-09-02 02:37:15 +0300

    Solved the circle_area challenge: all tests passing

--- demo.py
+++ demo.py
@@ -4,4 +4,10 @@
 
 def circle_area(radius):
     """Returns the area of the circle of given radius"""
-    pass  # YOUR CODE HERE
+import math  # keep import at the top
+
+def circle_area(radius):
+    """Returns the area of the circle of given radius"""
+    if radius <= 0:  # check for negative or zero radius
+        return 0
+    return math.pi * radius ** 2
